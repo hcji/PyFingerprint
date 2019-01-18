7146bbcd3bfdc29ef2e3f7634c0e242bf1308971
hcji <ji.hongchao@foxmail.com>
2019-01-18 11:06:46 +0800

    multi fingerprint

--- PyFingerprint/All_Fingerprint.py
+++ PyFingerprint/All_Fingerprint.py
@@ -5,6 +5,7 @@ Created on Mon Dec 10 10:11:11 2018
 @author: hcji
 """
 
+import numpy as np
 from PyFingerprint.CDK_Fingerprint import cdk_fingerprint
 from PyFingerprint.RDK_Fingerprint import rdk_fingerprint
 from PyFingerprint.Babel_Fingerprint import ob_fingerprint
@@ -26,4 +27,14 @@ def get_fingerprint(smi, fp_type, nbit=None, depth=None, output='bit'):
     else:
         raise IOError('invalid fingerprint type')
     return res
+
+def get_multi_fingerprint(smi, fp_types, nbit=None, depth=None, output='bit'):
+    vecs = [get_fingerprint(smi, f, nbit, depth, output='vector') for f in fp_types]
+    vec = np.concatenate(vecs)
+    if output == 'bit':
+        bits = np.where(vec > 0)[0]
+        return bits
+    else:
+        return vec
+
     
