c207255ccc97c2c66fbd5a7a7bef57f8516b2955
hcji <ji.hongchao@foxmail.com>
2019-05-20 10:42:51 +0800

    add MANIFEST

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup, find_packages
 
 setup(name='PyFingerprint',
-      version='0.1.0',
+      version='0.1.1',
       description='Python tool for generate fingerprints of a molecule',
       license='AGPLv3',
       author='Ji Hongchao',
       author_email='ji.hongchao@foxmail.com',
       url='https://github.com/hcji/PyFingerprint',
-      data_files = [('', ['PyFingerprint/CDK/cdk-2.2.jar'])],
+	  include_package_data = True,
       packages=find_packages()
      )
